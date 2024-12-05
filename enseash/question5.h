2cf5e1a8c747a5c0ff27ba2a585d51d8414fe095
jlblt <julien.blaret@ensea.fr>
2024-12-05 11:50:22 +0100

    Creation question 5

---
+++ enseash/question5.h
@@ -0,0 +1,16 @@
+//
+// Created by julien on 05/12/24.
+//
+
+#ifndef QUESTION5_H
+#define QUESTION5_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#define MAX_SIZE 6
+void welcome(void);
+void command_input(void);
+void command_execution(char *command_input);
+#endif //QUESTION5_H
