73d86689f31e40ab8b36b60df33345722ea2adab
Sédrenn Labrousse <161329715+Sedrennl@users.noreply.github.com>
2024-12-05 11:36:57 +0100

    Update question2.h

--- enseash/question2.h
+++ enseash/question2.h
@@ -7,10 +7,13 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
+
 #ifndef QUESTION2_H
 #define QUESTION2_H
 #endif //QUESTION2_H
+
 #define MAX_SIZE 6
+
 void welcome(void);
 void command_input(void);
 void command_execution(char *command_input);
