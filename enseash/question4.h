1fe8348c8bff8568df904ec2830a53b5e7812693
Sédrenn Labrousse <161329715+Sedrennl@users.noreply.github.com>
2024-12-06 15:38:13 +0100

    Update question4.h

--- enseash/question4.h
+++ enseash/question4.h
@@ -1,17 +1,20 @@
-//
-// Created by julien on 05/12/24.
+//TP Synthese ENSEA in the Shell
+//Question 3
+//Blaret julien and LABROUSSE Sédrenn on 06/12/24.
 //
 
 #ifndef QUESTION4_H
 #define QUESTION4_H
-
 #endif //QUESTION4_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
+
 #define MAX_SIZE 6
+
 void welcome(void);
 void command_input(void);
 void command_execution(char *command_input);
